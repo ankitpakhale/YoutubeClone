a48928b9381c8a66584c8a756527f79b9c4cfe6b
AnkitPakhale <akp3067@gmail.com>
2023-01-15 22:43:52 +0530

    Added: VideoCard Component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 
-const Videos = (props) => {
-  const { videos } = props;
-  // console.info(videos);
+const Videos = ({ videos }) => {
+  console.info(videos, "this is videos");
   return (
     <Stack direction={"row"} flexWrap="wrap" justifyContent={"start"} gap={2}>
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && <ChannelCard channelDetail={item} />}
-        </Box>
-      ))}
+      {videos
+        ? videos.map((item, idx) => (
+            <Box key={idx + 1}>
+              {item.id.videoId && <VideoCard video={item} />}
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+          ))
+        : console.info("null fetched")}
     </Stack>
   );
 };
