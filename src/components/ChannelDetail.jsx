4bc138a08554e9faf981948912a41c72791a6fe7
Ankit Pakhale <akp3067@gmail.com>
2023-01-16 17:50:29 +0530

    Added: SearchFeed

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,43 @@
 import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../utils/fetchFileFromAPI";
 import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight={"95vh"}>
+      <Box>
+        <div
+          style={{
+            background:
+              "radial-gradient(circle, rgba(255,140,8,1) 0%, rgba(255,75,75,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
